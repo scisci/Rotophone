79238fa252604f2f27fc66e8f4e1f275541384db
scisci <accounts.github@scisci.org>
2017-05-06 02:46:54 -0400

    added serial support

--- Rotophone/AppDelegate.m
+++ Rotophone/AppDelegate.m
@@ -8,14 +8,22 @@
 
 #import "AppDelegate.h"
 #import "MainViewController.h"
-//#import <ORSSerial/ORSSerial.h>
+#import "ORSSerialPortManager.h"
+#import "ORSSerialPort.h"
 
 @interface AppDelegate ()
 @property (retain) NSWindow* window;
 @property (retain) MainViewController* rootViewController;
+
+@property (retain) ORSSerialPortManager* serialPortManager;
+@property (retain) NSMutableArray* serialMenuItems;
+@property (unsafe_unretained) IBOutlet NSMenuItem *availablePortStartSeparator;
+@property (unsafe_unretained) IBOutlet NSMenuItem *noAvailablePortsMenuItem;
 @end
 
 @implementation AppDelegate
+
+static void *PersonAccountBalanceContext = &PersonAccountBalanceContext;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
@@ -34,14 +42,69 @@
     
     _window.contentView = _rootViewController.view;
     
+    _serialPortManager = [ORSSerialPortManager sharedSerialPortManager];
     
-    NSArray *ports = [[ORSSerialPortManager sharedSerialPortManager] availablePorts];
-    NSLog(@"Ports are %@", ports);
+    [_serialPortManager addObserver:self forKeyPath:@"availablePorts" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:PersonAccountBalanceContext];
+    
+    _serialMenuItems = [[NSMutableArray alloc] initWithCapacity:0];
+ 
+    [self handlePortsChanged];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void)handlePortsChanged {
+    NSArray *ports = _serialPortManager.availablePorts;
+    NSLog(@"Ports are %@", ports);
+    NSLog(@"ports changed");
+    
+    for (NSMenuItem *menuItem in _serialMenuItems) {
+        [menuItem.menu removeItem:menuItem];
+    }
+    
+    NSMenu *menu = _availablePortStartSeparator.menu;
+    NSInteger nextIndex = [menu indexOfItem:_availablePortStartSeparator] + 1;
+    
+    if (ports.count == 0) {
+        [_noAvailablePortsMenuItem setHidden:FALSE];
+    } else {
+        [_noAvailablePortsMenuItem setHidden:TRUE];
+        for (ORSSerialPort* port in ports) {
+            NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:port.name action:@selector(selectPort:) keyEquivalent:@""];
+            [menu insertItem:menuItem atIndex:nextIndex++];
+            [_serialMenuItems addObject:menuItem];
+        }
+    }
+    
+}
+
+- (void)selectPort:(id)sender {
+    NSMenuItem *menuItem = (NSMenuItem *)sender;
+    NSLog(@"Select port %@", menuItem.title);
+    for (NSMenuItem *menuItem in _serialMenuItems) {
+        [menuItem setState:NSOffState];
+    }
+    [menuItem setState:NSOnState];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+    
+    if (context == PersonAccountBalanceContext) {
+        // Do something with the balance…
+        [self handlePortsChanged];
+    } else {
+        // Any unrecognized context must belong to super
+        [super observeValueForKeyPath:keyPath
+                             ofObject:object
+                               change:change
+                              context:context];
+    }
 }
 
 
